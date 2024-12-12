17895eac9922bb4433817bfdc4dbc02c71b8830c
Ankit <777iamankit@gmail.com>
2024-12-12 16:25:14 +0530

    add mogodb

--- server/indexExpress.js
+++ server/indexExpress.js
@@ -18,10 +18,12 @@
 // import exp from 'constants';
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose  from 'mongoose';
 dotenv.config();
 
 import path from 'path';
 import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
 
 const app=express()
 
@@ -30,6 +32,9 @@ const __dirname=path.dirname(__filename);
 
 // const PORT =8000;
 const PORT=process.env.PORT;
+const URL=process.env.MONGODB_URI;
+console.log(URL);
+
 
 // app.get("/ankit",(req,res)=>{
 //   res.sendFile(path.join(__dirname,"views","calculator.html"));
@@ -37,6 +42,19 @@ const PORT=process.env.PORT;
 
 // app.get('/ankit')
 // app.use(express.static(path.join(__dirname,"views")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post("/name",(req,res)=>{
+  const fName=req.body.firstName;
+  const lName=req.body.secondName;
+  const fullName=fName+lName;
+  res.json({name:fullName});
+})
+
+
+
 
 app.use((req,res,next)=>{
   const secretcode=req.query.secret;
@@ -59,11 +77,22 @@ app.get("/",(req,res)=>{
     res.send("unathorized");
   }
 });
+
+
+
+mongoose.connect(URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>{
+  console.log("DB connected successfully");
+
+  app.listen(PORT,()=>{console.log(`server is running on http://localhost:${PORT}`)});
+
+})
+.catch((err)=>{
+  console.error("Connection failed:",err);
+})
+
+
+
 app.use(express.static(path.join(__dirname,"views")));
 
-
-
-
-
-app.listen(PORT,()=>{console.log(`server is running on http://localhost:${PORT}`)});
 
